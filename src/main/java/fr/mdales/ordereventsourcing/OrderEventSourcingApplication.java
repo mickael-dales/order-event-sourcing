a896f21bd575b17d2b24b04ac87ebb75b376c9b7
mdales <mdales@ippon.fr>
2019-02-09 11:33:09 +0100

    [ADD] Spring Boot init

---
+++ src/main/java/fr/mdales/ordereventsourcing/OrderEventSourcingApplication.java
@@ -0,0 +1,14 @@
+package fr.mdales.ordereventsourcing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderEventSourcingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderEventSourcingApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/fr/mdales/ordereventsourcing/OrderEventSourcingApplicationTests.java
@@ -0,0 +1,17 @@
+package fr.mdales.ordereventsourcing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrderEventSourcingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
